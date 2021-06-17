5e8bcaa8b5c3ecd3af8ac9929240ee42fdeeefab
SergeyHumeniuk <zirocull@ukr.net>
2021-06-17 21:49:59 +0300

    first sucsess

---
+++ newPostTest/index.php
@@ -0,0 +1,10 @@
+<?php
+require_once('../newPost/new_post.php');
+
+//use NewPostDelivery;
+
+$key = '0e48471e6ad3317b9454b05f7c3e6f82';
+$np = new NewPostApi();
+$result = $np->getDel($key);
+$res = json_decode($result);
+var_dump($res->data[0]->Ref);
---
+++ new_post.php
@@ -0,0 +1,47 @@
+<?php
+
+//namespace NewPostDelivery;
+
+/**
+ * Нова пошта.
+ *
+ * @author  SergeyHumeniuk 
+ *
+ * @see https://github.com/SergeyHumeniuk
+ */
+class NewPostApi
+{
+    //api from new post
+    //url for new post
+    const URL_NP = 'https://api.novaposhta.ua/v2.0/json/?';
+
+    public function getDel($key)
+    {
+
+        $params =  $this->Param($key);
+        $ch = curl_init(self::URL_NP . $params);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $html = curl_exec($ch);
+        curl_close($ch);
+
+        return $html;
+    }
+    public function Param($key)
+    {
+        $param = [
+            "apiKey" => $key,
+            "modelName" => "Counterparty",
+            "calledMethod" => "getCounterparties",
+            "methodProperties" => [
+                "CounterpartyProperty" => "Recipient",
+                "Page" => "1"
+            ]
+        ];
+        return json_encode($param);
+    }
+}
