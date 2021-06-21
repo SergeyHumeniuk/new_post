922437c7da941092aa1596870caa138ff88f1945
moorvin <zirocull@ukr.net>
2021-06-21 13:22:42 +0300

    add function getCity

--- new_post.php
+++ new_post.php
@@ -14,11 +14,28 @@ class NewPostApi
     //api from new post
     //url for new post
     const URL_NP = 'https://api.novaposhta.ua/v2.0/json/?';
-
+    //function for testinf
     public function getDel($key)
     {
 
         $params =  $this->Param($key);
+        $ch = curl_init(self::URL_NP . $params);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $html = curl_exec($ch);
+        curl_close($ch);
+
+        return $html;
+    }
+    //function for get city
+    public function getCity($key)
+    {
+
+        $params =  $this->GetCityParam($key);
         $ch = curl_init(self::URL_NP . $params);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
@@ -41,6 +58,15 @@ class NewPostApi
                 "CounterpartyProperty" => "Recipient",
                 "Page" => "1"
             ]
+        ];
+        return json_encode($param);
+    }
+    //params for get city
+    public function GetCityParam($key){
+        $param = [
+            "modelName" => "Address",
+            "calledMethod" => "getCities",
+            "apiKey" => $key,
         ];
         return json_encode($param);
     }
